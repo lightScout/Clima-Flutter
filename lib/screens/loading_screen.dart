fd49f30261d5552705c1f5c30d5e80a9a1a21ad2
lightScout <juanmullercs@hotmail.com>
2019-12-26 11:01:34 +0000

    Add Networking class Merg getLocation and getData

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,8 @@
-import 'package:clima/services/location.dart' as prefix0;
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+const apiKey = '611d816aea3e0c33588817559f6e20fe';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,19 +10,27 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double _latitude;
+  double _longitude;
+
   //Get location using Geolocator package
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.longitude);
-    print(location.latitude);
+    _longitude = location.longitude;
+    _latitude = location.latitude;
+
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$_latitude&lon=$_longitude&appid=$apiKey');
+
+    var decodedData = await networkHelper.getData();
   }
 
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
   @override
